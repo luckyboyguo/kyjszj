6523ce53285e767764ced6bfd047bd7c9386d7d5
luckyboyguo <luckyboyguo@126.com>
2020-03-16 16:46:49 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,7 +11,7 @@ module.exports = {
       { text: "单页", link: "/projects/" },
       { text: "分栏", link: "/guide/" },
       { text: "Markdown Demo", link: "/article/" },
-      { text: "仓库", link: "https://github.com/luckyboyguo/kyjszj" }
+      { text: "仓库", link: "https://github.com/weipxiu/vuePress" }
     ],
     sidebar: {
       // 侧边栏在 /guide/ 上
